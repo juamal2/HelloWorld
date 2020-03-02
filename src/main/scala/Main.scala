217addd5025c8a4a5966ceb1778c7d6e900fb744
juamal <juamal27@gmail.com>
2020-03-02 16:50:42 +0000

    completed return String completed return Anything completed return endXofString

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,25 @@
 object Main{
-
-  class Person(var name: String, age:Int)
+  def returnMessage(msg:String): String= msg
+  def returnAnything(item:Any) = item
+  def endXofString(string:String, x:Int)={
+    val str = string.substring(string.length()-x,string.length())
+  }
 
   def getDrink(drink:String): String ={
     "I am the last Line I am  ^^  the returned value"
   }
 
-  def isEven(number: Int): Boolean= if (number % 2 == 0) true else false
+  def isEven(number: Int): Boolean= {
+    if (number % 2 == 0)
+      true
+    else
+      false
+  }
 
   def main(args: Array[String]): Unit = {
-
-    var pepsi = "bottle" //Variable
-    val salad = "salad" //Constant
-
-    val person = new Person(name = "juamal", age = 21)
-
-
+    println(returnMessage("hello world"))
+    println(returnAnything("hello"))
+    println(endXofString("pepsi", 2))
   }
 
 
