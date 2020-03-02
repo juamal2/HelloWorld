78f6a278a880f5abed55ff1911ffc3bfa8615f88
juamal <juamal27@gmail.com>
2020-03-02 16:11:09 +0000

    after presentation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,31 @@
-object Main extends App{
+object Main{
+
+  class Person(var name: String, age:Int)
+
+  def getDrink(drink:String): String ={
+    "I am the last Line I am  ^^  the returned value"
+  }
+
+  def isEven(number: Int): Boolean= if (number % 2 == 0) true else false
+
+  def main(args: Array[String]): Unit = {
+
+    var pepsi = "bottle" //Variable
+    val salad = "salad" //Constant
+
+    val person = new Person(name = "juamal", age = 21)
 
 
-  println("Hello World")
+  }
+
+
+
+
 
 
 }
+
+
+
+
 
