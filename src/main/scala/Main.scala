d0c1179943f0ffe0f4f4f8f4e24797a598c966b4
juamal <juamal27@gmail.com>
2020-03-02 17:08:46 +0000

    completed return String completed return Anything completed return endXofString

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,14 @@ object Main{
   def endXofString(string:String, x:Int)={
     val str = string.substring(string.length()-x,string.length())
   }
-
-  def getDrink(drink:String): String ={
-    "I am the last Line I am  ^^  the returned value"
+  def superMulti(a:Int, b:Int, c:Int )={
+    var nums = List(a,b,c)
+    nums = nums.sorted
+    (nums(0) + nums(1))*nums(2)
+  }
+  def twoStringReplace(str1:String, str2:String, char1:Char, char2:Char) ={
+    var str = str1 + str2
+    str.replace(char1, char2)
   }
 
   def isEven(number: Int): Boolean= {
@@ -20,6 +25,8 @@ object Main{
     println(returnMessage("hello world"))
     println(returnAnything("hello"))
     println(endXofString("pepsi", 2))
+    println(superMulti(2,7,4))
+    println(twoStringReplace("pepsi", "bopple", 'p', 't'))
   }
 
 
